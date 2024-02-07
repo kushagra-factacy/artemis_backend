7f50c3aafe95c378f6ef6a882e9662b495745007
Ubuntu <azureuser@Artemis.l3yetuhinuke5giqzqykfugfac.cx.internal.cloudapp.net>
2024-02-07 04:00:09 +0000

    firest commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+
+require('dotenv').config();
+
+const app= express();
+app.get('/',(req,res)=>{
+    res.send("hello");
+})
+
+
+
+app.listen(process.env.PORT ,()=>{
+    console.log(`listening on on port ${process.env.PORT}`);
+});
+
